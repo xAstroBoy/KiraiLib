67113b67f8c9382fa7dd83c56c4f6b36a85ef10a
xKiraiChan <xKiraiChan@gmail.com>
2021-02-21 21:05:43 -0500

    add constants class

---
+++ KiraiLib/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiraiMod
+{
+    public partial class KiraiLib
+    {
+        /// <summary> Useful constant values </summary>
+        public static class Constants
+        {
+            /// <summary> Moves back a character in the console when \b doesn't work </summary>
+            public const string VT_BACKSPACE = "\x1b[1D";
+            /// <summary> Replaces the previous character in the console with a space </summary>
+            public const string VT_ERASE = VT_BACKSPACE + " ";
+        }
+    }
+}
