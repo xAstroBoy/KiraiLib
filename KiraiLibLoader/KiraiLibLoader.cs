9bcd6efb49eb8113f63169fa6c9491db775618a9
xKiraiChan <xKiraiChan@gmail.com>
2021-03-16 00:31:04 -0400

    add caching to lib loader

--- KiraiLibLoader/KiraiLibLoader.cs
+++ KiraiLibLoader/KiraiLibLoader.cs
@@ -3,6 +3,8 @@ using System;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace KiraiMod
@@ -12,37 +14,75 @@ namespace KiraiMod
         private static bool loaded = false;
         public static bool hasErrored = false;
 
-        static KiraiLibLoader()
-        {
-            Load();
-        }
-
+        static KiraiLibLoader() => Load();
+        public static void Load() => LoadEx();
         public static bool LoadEx()
         {
-            if (!loaded)
+            if (loaded)
+                return false;
+            else loaded = true;
+
+            if (AppDomain.CurrentDomain.GetAssemblies().Any(a => a.GetName().Name == "KiraiLib"))
+                return false;
+
+            MelonLogger.Msg("------------------------------");
+
+            if (!System.IO.Directory.Exists("Dependencies"))
+                System.IO.Directory.CreateDirectory("Dependencies");
+
+            HttpClient http = new HttpClient();
+
+            Task<string> hashRequest = http.GetStringAsync("https://github.com/xKiraiChan/KiraiLib/raw/master/Dist/KiraiLib.hash");
+
+            byte[] bytes = null;
+            string oHash = null;
+            if (System.IO.File.Exists("Dependencies/KiraiLib.dll"))
             {
-                loaded = true;
+                bytes = System.IO.File.ReadAllBytes("Dependencies/KiraiLib.dll");
+                oHash = SHA256(bytes);
+            }
 
-                if (AppDomain.CurrentDomain.GetAssemblies().Any(a => a.GetName().Name == "KiraiLib")) 
-                    return hasErrored;
+            hashRequest.Wait();
+            if (hashRequest.Exception is null)
+            {
+                string hash = new Regex("[^a-zA-Z0-9]").Replace(hashRequest.Result, "");
 
-                Task<byte[]> req = new HttpClient().GetByteArrayAsync("https://github.com/xKiraiChan/KiraiLib/raw/master/Dist/KiraiLib.dll");
+                MelonLogger.Msg(oHash);
+                MelonLogger.Msg(hash);
 
+                if (hash != oHash)
+                {
+                    oHash = hash;
+
+                    Task<byte[]> libRequest = http.GetByteArrayAsync("https://github.com/xKiraiChan/KiraiLib/raw/master/Dist/KiraiLib.dll");
+
+                    libRequest.Wait();
+                    if (libRequest.Exception is null)
+                    {
+                        bytes = libRequest.Result;
+                        System.IO.File.WriteAllBytes("Dependencies/KiraiLib.dll", bytes);
+                        MelonLogger.Msg("KiraiLib updated");
+                    }
+                    else hasErrored = true;
+                }
+                else MelonLogger.Msg("KiraiLib up to date");
+            }
+            else
+            {
+                MelonLogger.Msg("Failed to check hash");
+                hasErrored = true;
+            }
+
+            if (bytes != null)
+            {
                 try
                 {
-                    byte[] bytes = req.Result;
-
                     Assembly.Load(bytes);
 
-                    new Action(() => { 
-                        MelonLogger.Msg($"KiraiLib: ({KiraiLib.CRC32.Hash(bytes)})");
+                    new Action(() =>
+                    {
+                        MelonLogger.Msg($"KiraiLib: ({oHash})");
                     })();
-                }
-                catch (AggregateException)
-                {
-                    hasErrored = true;
-
-                    MelonLogger.Msg($"Failed to load KiraiLib: {req.Exception.Message}");
                 }
                 catch (Exception ex)
                 {
@@ -50,16 +90,21 @@ namespace KiraiMod
 
                     MelonLogger.Msg($"Failed to load KiraiLib: {ex.Message}");
                 }
-
-                if (!hasErrored)
-                    MelonLogger.Msg($"Successfully loaded KiraiLib");
-
-                MelonLogger.Msg("------------------------------");
             }
+
+            MelonLogger.Msg("------------------------------");
 
             return hasErrored;
         }
 
-        public static void Load() => LoadEx();
+        public static string SHA256(byte[] bytes)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(bytes);
+            foreach (byte theByte in crypto)
+                hash.Append(theByte.ToString("X2"));
+            return hash.ToString();
+        }
     }
 }
