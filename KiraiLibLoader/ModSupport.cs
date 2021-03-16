ff770f0afd49c11963381f319e75e529537cc8af
xKiraiChan <xKiraiChan@gmail.com>
2021-03-16 15:32:19 -0400

    use constant in build version instead of hardcoding

--- KiraiLibLoader/ModSupport.cs
+++ KiraiLibLoader/ModSupport.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(KiraiMod.KiraiLibLoader.ModSupport), "KiraiLibLoader", "\x1b[1D ", "Kirai Chan#8315", "github.com/xKiraiChan/KiraiLib")]
+[assembly: MelonInfo(typeof(KiraiMod.KiraiLibLoader.ModSupport), "KiraiLibLoader", KiraiMod.KiraiLib.Constants.VT_ERASE, "Kirai Chan#8315", "github.com/xKiraiChan/KiraiLib")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace KiraiMod
