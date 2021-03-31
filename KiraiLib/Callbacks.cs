e2181b94ac01a7eb02a8f4279613a45ed69a8ed0
xKiraiChan <xKiraiChan@gmail.com>
2021-03-31 19:00:03 -0400

    add compatibility

--- KiraiLib/Callbacks.cs
+++ KiraiLib/Callbacks.cs
@@ -6,15 +6,22 @@ namespace KiraiMod
 {
     public static partial class KiraiLib
     {
+        [Obsolete("Use Events instead", false)]
+        public static class Callbacks
+        {
+            public static Action OnUIUnload = new Action(() => { });
+            public static Action OnUIReload = new Action(() => { });
+        }
+
         /// <summary> All the events that can be subscribed to </summary>
         /// <example> Callbacks.OnReload += new Action(() => { /* ... */ }) </example>
         public static class Events
         {
             /// <summary> Called after all UI elements have been destroyed </summary>
-            public static Action OnUIUnload = new Action(() => { });
+            public static Action OnUIUnload = new Action(() => Callbacks.OnUIUnload());
 
             /// <summary> Called when it is time to recreate UI elements </summary>
-            public static Action OnUIReload = new Action(() => { });
+            public static Action OnUIReload = new Action(() => Callbacks.OnUIReload());
 
             public static Action<int, string> OnSceneLoad = new Action<int, string>((_, __) => { });
             public static Action<Player, VrcEvent> OnRPC = new Action<Player, VrcEvent>((_, __) => { });
